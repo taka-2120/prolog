2a0fbbacf7ed961e95eb3a1daa12f8eb4f559182
taka-2120 <yu_tkhs@icloud.com>
2024-05-17 16:54:01 +0900

    fix: r04

--- r04/p01.pl
+++ r04/p01.pl
@@ -1,4 +1,4 @@
 pos_neg([], [], []).
-pos_neg([H|T], PL, [H|NL]) :- H < 0, pos_neg(T, PL, NL).
-pos_neg([H|T], [H|PL], NL) :- H > 0, pos_neg(T, PL, NL).
-pos_neg([_|T], PL, NL) :- pos_neg(T, PL, NL).
+pos_neg([X|X1], [X|PL], NL) :- X > 0, pos_neg(X1, PL, NL).
+pos_neg([X|X1], PL, [X|NL]) :- X < 0, pos_neg(X1, PL, NL).
+pos_neg([_|X1], PL, NL) :- pos_neg(X1, PL, NL).
