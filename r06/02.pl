392c23482d1366695a9c6cd09b91edf4a4d18e56
taka-2120 <yu_tkhs@icloud.com>
2024-06-07 15:38:13 +0900

    fix: count_ocr

--- r06/02.pl
+++ r06/02.pl
@@ -1,6 +1,7 @@
 count_ocr(A, A, 1).
 count_ocr(A, f(T), X) :- count_ocr(A, T, X).
 count_ocr(A, g(L, R), X) :- count_ocr(A, L, X1), count_ocr(A, R, X2), X is X1 + X2.
+count_ocr(_, _, 0).
 
 test1(X) :- count_ocr(a, f(g(a, f(a))), X).
 test2(X) :- count_ocr(b, f(g(a, f(a))), X).
--- r07/2.pl
+++ r07/2.pl
@@ -1,11 +1,8 @@
 count_ocr(A, A, 1).
 count_ocr(A, f(T), X) :- count_ocr(A, T, X).
 count_ocr(A, g(L, R), X) :- count_ocr(A, L, X1), count_ocr(A, R, X2), X is X1 + X2.
+count_ocr(_, _, 0).
 
-count_alphabet(a, _).
-count_alphabet(b, _).
-count_alphabet(0, _).
-count_alphabet(1, _).
 count_alphabet(A, C) :- count_ocr(a, A, C1), count_ocr(b, A, C2), C is C1 + C2.
 
 test(C) :- count_alphabet(g(0, f(g(b, f(a)))), C).
