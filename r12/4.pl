ca9ff4a66ffcd9697f84cbbef81dea47daf921c3
taka-2120 <yu_tkhs@icloud.com>
2024-07-18 21:22:38 +0900

    fix: r12.4

--- r12/4.pl
+++ r12/4.pl
@@ -23,20 +23,14 @@ a([(2,3),(1,3),(2,2),(3,3),(3,2),(3,1),(2,1),(1,1),(1,2)]).
 c([(2,2),(1,3),(3,2),(2,3),(3,3),(3,1),(2,1),(1,1),(1,2)]).
 goal([(2,2),(1,3),(2,3),(3,3),(3,2),(3,1),(2,1),(1,1),(1,2)]).
 
-
 swap(E, T, [E | L1], [T | L1]).
 swap(E, T, [T | L1], [E | L1]).
 swap(E, T, [H | L1], [H | L2]) :- swap(E, T, L1, L2).
 edge([E | S1], [T | S2]) :- swap(E, T, S1, S2).
 
-ts(T, L2) :- swap((2,3),T,[(1,3),(2,2),(3,3),(3,2),(3,1),(2,1),(1,1),(1,2)],L2).
-te :- edge([(2,3),(1,3),(2,2),(3,3),(3,2),(3,1),(2,1),(1,1),(1,2)], S2), write(S2).
+path(S, G, [G]) :- edge(S, G).
+path(S, G, [S|T]) :- edge(S, S1), path(S1, G, T).
 
-min_node(Srt, C) :- findall(Cost, edge(Srt, _, Cost), CostList), min_member(MinCost, CostList), edge(Srt, C, MinCost).
-
-path(A, A, [A]).
-path(A, B, [A|T]) :- min_node(A, C), path(C, B, T).
-
-
-
-hc_puzzle(Path) :- path(s, g, Path).
+hc_puzzle(Path) :- goal(G), a(A), path(A, G, Path).
+% hc_puzzle(Path) :- goal(G), c(C), path(C, G, Path).
+test :- hc_puzzle(Path), write(Path).
